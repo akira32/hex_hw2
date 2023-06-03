5a347f732ecd88ab9b7f2bb8f1355c180ab7bcdb
MSI\akira <rikcuo.akira@gmail.com>
2023-06-03 15:00:25 +0800

    tag

--- js/all.js
+++ js/all.js
@@ -1,3 +1,26 @@
+
+$(document).ready(function() {
+  // 開起選單
+  $('.dropdown-btn').click(function(e) {
+    $('.dropdown-menu').toggleClass('show');
+  });
+
+  // 切換按鈕文字
+  $('.new-to-old').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text($('.new-to-old').text());
+  });
+
+  $('.old-to-new').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text($('.old-to-new').text());
+  });
+});
+
+// .text() 運用：https://www.w3schools.com/jquery/html_text.asp
+
 // function myMove(elem,late)
 // {
 //   let id = null;
